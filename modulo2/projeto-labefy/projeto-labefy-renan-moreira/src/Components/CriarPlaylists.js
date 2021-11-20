84a83cb93ac16892a0be331b47f4c8562cef474c
RenanRodrigues02 <devrenan02@gmail.com>
2021-11-19 21:14:22 -0300

    Entrega do dia 19/11 - continuacao projeto-labefy

--- modulo2/projeto-labefy/projeto-labefy-renan-moreira/src/Components/CriarPlaylists.js
+++ modulo2/projeto-labefy/projeto-labefy-renan-moreira/src/Components/CriarPlaylists.js
@@ -1,16 +1,49 @@
 import React from "react"
 import axios from "axios";
+import styled from "styled-components";
+
+const CardInicial = styled.div`
+border: 1px solid black;
+padding: 10px;
+margin:  10px;
+width: 300px;
+display:flex;
+justify-content:space-between;
+
+`
+
 
 export default class CriarPlaylists extends React.Component {
 
     state = {
         
-        nomePlaylist:""
+        nomePlaylist:"",
+        minhasMusicas: [],
+        nomeMusica:"",
+        artistaMusica:"",
+        url:"",
+        musicaId: ""
     }
 
-        playlistsNomes = (event) =>{
-            this.setState({nomePlaylist:event.target.value})
-        }
+    onChangeID = (event) =>{
+      this.setState({musicaId: event.target.value})
+    
+    }
+
+    onChangePlaylist = (e) => {
+      this.setState({ nomePlaylist: e.target.value });
+    
+    };
+
+       onChangeMusica = (e) => {
+    this.setState({ nomeMusica: e.target.value });
+  
+  };
+  onChangeArtistaMusica = (e) => {
+    this.setState({ artistaMusica: e.target.value });
+  
+  };
+
 
     criaPlaylists = () => {
         const body = {
@@ -36,19 +69,66 @@ export default class CriarPlaylists extends React.Component {
           });
       };
       
-    //   adicionarPlaylist = () =>{
-    //       console.log (this.state)
-    //   }
+      adicionarMusica = (id) => {
+        const body = {
+          name: this.state.nomeMusica,
+          artist: this.state.artistaMusica,
+          url:"http://spoti4.future4.com.br/1.mp3"
+        };
+        axios
+          .post(
+            `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`,
+            body,
+            {
+              headers: {
+                Authorization: "renan-rodrigues-carver"
+              }
+            }
+          )
+          .then(() => {
+            alert ("Adiconada com sucesso!")
+            this.setState ({nomeMusica:"", artistaMusica:"", url:""})
+            console.log(this.setState)
+          })
+          .catch((err) => {
+              alert (err.response.data.message)
+            console.log(err.response);
+          });
+      };
 
     render () {
 
+    //   const listaMusicas = this.state.minhasMusicas.map((music) => {
+              
+    //     return (
+    //     <CardInicial key={music.id}> {music.artist}-{music.name} <button onClick={()=> this.adicionarMusica(music.id)}>Adicionar</button></CardInicial>
+        
+
+    //     )
+        
+      
+    // })
+
+      
         return (
             <div>
                 <button onClick ={this.props.irParaVerPlaylists}> Acessar Playlists</button>
+
+                
                 <h2> Crie sua PlayList personalizada!</h2>
-                <input placeholder ={"Nome da Playlist"} value ={this.state.nomePlaylist} onChange ={this.playlistsNomes} />
+                <input placeholder ={"Nome da Playlist"} value ={this.state.nomePlaylist} onChange ={this.onChangePlaylist} />
                
                 <button onClick = {this.criaPlaylists}> Adicionar</button>
+
+                <h2> Adicione Artista</h2>
+                <input placeholder ={"Nome do Artista"} value ={this.state.nomeMusica} onChange ={this.onChangeMusica} />
+               
+                <button onClick = {this.adicionarMusica}> Adicionar</button>
+                <h2> Adicione Musica</h2>
+                <input placeholder ={"Nome da Música"} value ={this.state.artistaMusica} onChange ={this.onChangeArtistaMusica} />
+               
+                <button onClick = {this.adicionarMusica}> Adicionar</button>
+                {/* {listaMusicas} */}
                 
 
 
--- modulo2/projeto-labefy/projeto-labefy-renan-moreira/src/Components/VerPlaylists.js
+++ modulo2/projeto-labefy/projeto-labefy-renan-moreira/src/Components/VerPlaylists.js
@@ -6,20 +6,33 @@ const CardPlaylists = styled.div`
 border: 1px solid black;
 padding: 10px;
 margin:  10px;
+width: 300px;
 display:flex;
-
+justify-content:space-between;
 
 `
 export default class VerPlaylists extends React.Component {
 
     state = {
-        minhasPlaylists:[]
+        minhasPlaylists:[],
+        playlistId:""
+        
+       
     }
 
    componentDidMount () {
        this.mostraPlaylists()
    }
    
+   
+
+   onChangeID = (event) =>{
+    this.setState({playlistId: event.target.value})
+  
+  }
+
+
+
     mostraPlaylists = (id) => {
             const url ="https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
             axios.get(url,{
@@ -28,7 +41,7 @@ export default class VerPlaylists extends React.Component {
                 }
             })
             .then((res) => {
-              console.log (this.setState)
+            //   console.log (this.setState)
                 this.setState({minhasPlaylists:res.data.result.list})
              
               })
@@ -38,23 +51,71 @@ export default class VerPlaylists extends React.Component {
               });
           };
 
-    
+          deletaPlaylists = (id) => {
+            const url =`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
+            
+            axios.delete(url,{
+                headers: {
+                    Authorization: "renan-rodrigues-carver"
+                }
+            })
+            .then((res) => {
+                alert ("Playlist deletada com sucesso!")
+              
+              this.mostraPlaylists()
+            //   this.setState({minhasPlaylists:res.data.result.list})
+            console.log (res)
+             
+              })
+              .catch((err) => {
+                //   alert("Erro encontrado, tente novamente!")
+                console.log(err.response.data)
+               
+              });
+          };
+
+         
+
+
         
           render (){
             const listaPlaylists = this.state.minhasPlaylists.map((itens) => {
               
-                return (<CardPlaylists key={itens.id}> {itens.nomePlaylist} - {itens.id} </CardPlaylists>)
+                return (
+                <CardPlaylists key={itens.id}> {itens.name} <button onClick={()=> this.deletaPlaylists(itens.id)}>X</button> <button>Adicionar música</button> </CardPlaylists>
+                
+            
+                
+
+                )
+                
               
             })
+          
+                              
 
-            console.log (listaPlaylists)
             return (
                 <div>
                     <button onClick ={this.props.irParaInformacoesPlaylists}> Detalhes da Playlist</button>
-                        <h2> Minhas Playlists</h2> 
-                                    
+                        <h2> Minhas Playlists</h2>     
                                 {listaPlaylists}
-                              
+
+
+                
+
+
+
+             {/* <button onClick={this.mostraPlaylists}>  Playlists Cadastradas  </button>
+                {
+                this.state.minhasPlaylists.map((adicionadas) => {
+                if (adicionadas.name){
+                    return <CardPlaylists key={adicionadas.id}>{adicionadas.name} <button onClick={()=> this.deletaPlaylists(adicionadas.id)}>X</button></CardPlaylists>
+                }
+
+
+})
+
+}         */}
 
 
                 </div>
