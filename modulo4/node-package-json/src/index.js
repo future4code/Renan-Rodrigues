b92319d45afecc1469c7aecaa1e1b84e39250590
RenanRodrigues02 <devrenan02@gmail.com>
2022-01-18 19:42:51 -0300

    exercicio-node-package-json- 18/01

---
+++ modulo4/node-package-json/src/index.js
@@ -0,0 +1,42 @@
+// 1 a) por meio do "process.argv[]"
+
+//1 b)
+
+// const meuNome = "Renan"
+
+// const minhaIdade = "25"
+
+// console.log ("Olá,", meuNome,"!", "Você tem", minhaIdade, "anos")
+
+// 1 c)
+// const meuNome = "Renan"
+// const idadeAtual = Number ("25")
+// const idadeFutura = Number ("7")
+
+// console.log (`Olá,${meuNome}! Você tem ${idadeAtual} anos. Em sete anos você terá ${idadeAtual + idadeFutura}`)
+
+// 2)
+
+// console.log (process.argv[0])
+// console.log (process.argv[1])
+// console.log (process.argv[2])
+// console.log (process.argv[3])
+
+// const a = Number (process.argv[2])
+// const b = Number (process.argv[3])
+
+// const multiplicar = (a * b)
+
+// console.log(multiplicar)
+
+
+//3)
+
+// tarefas = [
+//     "Beber água"
+// ]
+
+
+// tarefas.push ("correr na praia")
+
+// console.log (tarefas)
