9471c7f1a5ec131c63cbbdfba1d7423720a71d1e
RenanRodrigues02 <devrenan02@gmail.com>
2022-01-19 17:06:37 -0300

    intro-type-entrega dia 19/01

---
+++ modulo04/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,18 @@
+
+const mensagem = "Olá, teste"
+
+console.log (mensagem)
+
+//1)
+
+// function checaTriangulo(a:number, b:number, c:number) {
+//     if (a !== b && b !== c) {
+//       return "Escaleno";
+//     } else if (a === b && b === c) {
+//       return "Equilátero";
+//     } else {
+//       return "Isósceles";
+//     }
+//   }
+
+//   console.log (checaTriangulo(3,3,3))
