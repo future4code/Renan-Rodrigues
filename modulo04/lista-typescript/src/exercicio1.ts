4fba864a7af8c48331410aa60ce9514262f70d44
RenanRodrigues02 <devrenan02@gmail.com>
2022-01-21 18:39:13 -0300

    entrega dia 21/01

---
+++ modulo04/lista-typescript/src/exercicio1.ts
@@ -0,0 +1,12 @@
+
+//
+
+// const nome:string = "Renan"
+// const dataNascimento: string = "24/04/1993"
+
+// function info (nome?:string, dataNascimento?:string):void  {
+//     console.log (`Olá, me chamo ${nome}`)
+    
+// }
+
+// console.log (`Olá, me chamo ${nome}, nasci em ${dataNascimento}`)
