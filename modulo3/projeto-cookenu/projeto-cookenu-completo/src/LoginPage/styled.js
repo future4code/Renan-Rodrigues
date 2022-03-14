ae151a10c4765f4773c470c3361a7a6f6b0ac81a
RenanRodrigues02 <devrenan02@gmail.com>
2022-03-14 12:03:18 -0300

    atualizacoes

--- modulo3/projeto-cookenu/projeto-cookenu-completo/src/LoginPage/styled.js
+++ modulo3/projeto-cookenu/projeto-cookenu-completo/src/LoginPage/styled.js
@@ -4,11 +4,21 @@ export const ScreenContainer = styled.div`
   display: flex;
   flex-direction: column;
 
-  
   align-items: center;
   width: 100vw;
-  margin-top: 5vh;
-  margi-bottom: 10vh;
+  margin-top: 50vh;
+  margin-bottom: 10vh;
+  
+  @media (max-width:420px){
+    position:absolute;
+    bottom:30px;
+  }
+
+  @media (max-width:380px){
+    position:absolute;
+    bottom:10px;
+    
+  }
   
 `
 
@@ -39,13 +49,36 @@ export const SignUpButtonContainer = styled.div`
 `
 
 export const LogoImage = styled.img`
-  width: 70vw;
+  position:absolute;
+  width: 250px;
   max-width: 300px;
-  margin-right: 800px;
-  margin-left: 120vh;
+  bottom:280px;
+  /* margin-right: 800px; */
+  margin-left: 73vh;
   margin-bottom: 3vh;
   border-radius: 20px;
    
+  /* @media (max-width:420px){
+    margin-left: 108vh;
+
+  } */
+  @media (max-width:420px){
+    position:absolute;
+    width:250px;
+    margin-left:70px;
+    bottom:360px;
+
+  }
+  @media (max-width:380px){
+    position:absolute;
+    width:200px;
+    bottom:320px;
+    margin-left:75px;
+    
+
+  }
+  
+  
   
 
   `
